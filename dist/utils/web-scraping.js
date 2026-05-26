82f32507dd02c3b01047b3ff0bddfe9a7097125a
unknown <adityadgr29@gmail.com>
2026-05-26 12:38:40 +0530

    forgot to remove the testing file

--- dist/utils/web-scraping.js
+++ dist/utils/web-scraping.js
@@ -1,8 +1,7 @@
 // npm install @mendable/firecrawl-js
 import "dotenv/config";
-// firecrawlKey=fc-3d43f2164347464db5aef806d8b989cb 
 import Firecrawl from '@mendable/firecrawl-js';
-const firecrawl = new Firecrawl({ apiKey: "fc-3d43f2164347464db5aef806d8b989cb " });
+const firecrawl = new Firecrawl({ apiKey: process.env.firecrawlKey });
 const results = await firecrawl.search('jpmc careers or jobs', {
     limit: 3,
 });
